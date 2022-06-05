6b9e9633fa8d2df0f26c003ce93ab6eb48ad3e5a
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-05 11:39:08 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = "0px";
+const game = document.getElementById("game");
+const gameWidthNumbers = parseInt(game.style.width.replace("px", ""), 10);
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left >= 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left <= 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if(e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
